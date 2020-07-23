df5339cb8f02a7eb8b91fc37086333c3d7b14ec8
ParthVarde <parthvarde50@gmail.com>
2020-07-23 14:26:03 +0530

    Added the Solution

---
+++ EmployeeManagement/Models/MockEmployeeRepository.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Models
+{
+    public class MockEmployeeRepository : IEmployeeRepository
+    {
+        private List<Employee> _employeesList;
+
+        public MockEmployeeRepository()
+        {
+            _employeesList = new List<Employee>()
+            {
+                new Employee { Id = 1, Name = "Parth", Email = "abc@gmail.com", Department = Dept.IT},
+                new Employee { Id = 2, Name = "Dhaval", Email = "def@gmail.com", Department = Dept.HR},
+                new Employee { Id = 3, Name = "Dharmik", Email = "ghi@gmail.com", Department = Dept.Marketing},
+            };
+        }
+
+        public IEnumerable<Employee> GetAllEmployee()
+        {
+            return _employeesList;
+        }
+
+        public Employee GetEmployee(int Id)
+        {
+            return _employeesList.FirstOrDefault(x => x.Id == Id);
+        }
+
+        public Employee Add(Employee employee)
+        {
+            employee.Id = _employeesList.Max(x => x.Id) + 1;
+            _employeesList.Add(employee);
+            return employee;
+        }
+
+        public Employee Update(Employee employeeChanges)
+        {
+            Employee employee = _employeesList.FirstOrDefault(x => x.Id == employeeChanges.Id);
+            if (employee != null)
+            {
+                employee.Name = employeeChanges.Name;
+                employee.Email = employeeChanges.Email;
+                employee.Department = employee.Department;
+            }
+            return employee;
+        }
+
+        public Employee Delete(int id)
+        {
+            Employee employee = _employeesList.FirstOrDefault(x => x.Id == id);
+            if(employee != null)
+            {
+                _employeesList.Remove(employee);
+            }
+            return employee;
+        }
+    }
+}
---
+++ EmployeeManagement/Startup.cs
@@ -0,0 +1,55 @@
+using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace EmployeeManagement
+{
+    public class Startup
+    {
+        private IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContextPool<AppDbContext>(options => options.UseSqlServer(_config.GetConnectionString("EmployeeDBConnection")));
+
+            services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>();
+
+            services.AddMvc(options => options.EnableEndpointRouting = false);
+            services.AddScoped<IEmployeeRepository, SQLEmployeeRepository>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseStatusCodePagesWithRedirects("/Error/{0}");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseAuthentication();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
